5726787e592018fbd679aa1840e2d28b728ba7fa
Scott Thorpe <sthorpe@gmail.com>
2018-09-11 14:01:04 -0400

    Learning c programming temp file because vscode is weird

--- prime.c
+++ prime.c
@@ -1,21 +1,7 @@
 #include <stdio.h>
 
-int main() {
-  int n, i, c = 0;
-  printf("Enter any number n:");
-  scanf("%d", &n);
-
-  /*logic*/  for (i = 1; i <= n; i++) {
-      if (n % i == 0) {
-         c++;
-      }
-  }
-
-  if (c == 2) {
-  printf("n is a Prime number");
-  }
-  else {
-  printf("n is not a Prime number");
-  }
+int main()
+{
+  printf("Hi, my name is Scott");
   return 0;
 }
